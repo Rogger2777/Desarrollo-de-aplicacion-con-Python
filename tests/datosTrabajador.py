4513c9e79f36bbe6b5a024fd35b1427fd2e736a4
Rogger <750490794continental.edu.pe>
2023-12-06 11:23:21 -0500

    se implemento los tests de prueba

---
+++ tests/datosTrabajador.py
@@ -0,0 +1,15 @@
+trabajadores_data = [
+    {'nombre': 'Luis', 'sueldo': 2000, 'mesanio': 'February 2023'},
+    {'nombre': 'Ana', 'sueldo': 3000, 'mesanio': 'March 2023'},
+    {'nombre': 'Roberto', 'sueldo': 4000, 'mesanio': 'April 2023'},
+    {'nombre': 'Carmen', 'sueldo': 2500, 'mesanio': 'May 2023'},
+    {'nombre': 'Alberto', 'sueldo': 3500, 'mesanio': 'June 2023'},
+    {'nombre': 'Sofía', 'sueldo': 4500, 'mesanio': 'August 2023'},
+    {'nombre': 'Ricardo', 'sueldo': 2750, 'mesanio': 'September 2023'},
+    {'nombre': 'Elena', 'sueldo': 3750, 'mesanio': 'October 2023'},
+    {'nombre': 'Hugo', 'sueldo': 4800, 'mesanio': 'November 2023'},
+    {'nombre': 'Isabel', 'sueldo': 1900, 'mesanio': 'December 2023'}
+
+]
+
+
